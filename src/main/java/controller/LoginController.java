f53b66adaf64dc299358a3eab1d7dcec893fa8a2
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2023-07-02 23:13:01 +0530

    create main

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,6 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
 
+    @FXML
+    private TextField userName;
+    static String User_name;
+
+    @FXML
+    void onAction(ActionEvent event) throws IOException {
+        User_name=userName.getText();
+        userName.clear();
+        Stage stage=new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(LoginController.class.getResource("/view/client.fxml"))));
+        stage.close();
+        stage.centerOnScreen();
+        stage.show();
+
+    }
 
 }
---
+++ src/main/java/server/Main.java
@@ -0,0 +1,24 @@
+package server;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root= FXMLLoader.load(this.getClass().getResource("/view/login.fxml"));
+        Scene mainScene=new Scene(root);
+        stage.setScene(mainScene);
+        stage.setTitle("Java FX POS");
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
