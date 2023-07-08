3ad83e69ab70ff261c25b35572c24272134f5e9d
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2023-07-08 22:52:03 +0530

    complete client interface

--- src/main/java/controller/ClientController.java
+++ src/main/java/controller/ClientController.java
@@ -1,4 +1,119 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class ClientController {
+
+    @FXML
+    private AnchorPane emojiPane;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private TextField txtCht;
+
+    @FXML
+    private ScrollPane txtMsgShow;
+
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    void bigSmile(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnCloseOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnEmojiOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnGalleryOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSendMsgOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void emojiPaneOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void heart(MouseEvent event) {
+
+    }
+
+    @FXML
+    void love(MouseEvent event) {
+
+    }
+
+    @FXML
+    void money(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mouseOnClickHideEmoji(MouseEvent event) {
+
+    }
+
+    @FXML
+    void oneEye(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sad(MouseEvent event) {
+
+    }
+
+    @FXML
+    void smalleSmile(MouseEvent event) {
+
+    }
+
+    @FXML
+    void teethSmile(MouseEvent event) {
+
+    }
+
+    @FXML
+    void tongSmile(MouseEvent event) {
+
+    }
+
+    @FXML
+    void tuin(MouseEvent event) {
+
+    }
+
+    @FXML
+    void txtChtOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verySad(MouseEvent event) {
+
+    }
+
 }
